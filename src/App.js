3464b03b9c2a93f30a0179d1cd27d65f4cbbb8d2
Mat Ruane <mat@ruane.net.nz>
2021-12-02 20:25:17 +1300

    persons' names can be added to the phonebook

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React, { useState } from "react";
+import React, { useState } from 'react'
+import Person from './components/Person'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
+  const [newName, setNewName] = useState('')
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+    }
+
+    setPersons(persons.concat(personObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input 
+                  value={newName}
+                  onChange={handleNameChange}  
+                />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => <Person key={person.name} person={person} />)}
+      </ul>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/Person.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Person = ({ person }) => {
+
+  return (
+    <li>{person.name}</li>
+  )
+}
+
+export default Person
